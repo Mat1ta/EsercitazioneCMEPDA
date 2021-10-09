ad0a09cda401d3f85a030eaaefc2aa2eb4e5e607
Mat1ta <carminatimatilde@gmail.com>
2021-10-09 10:45:09 +0200

    Initial Import.

--- FrequenzaAlfabeto.py
+++ FrequenzaAlfabeto.py
@@ -3,13 +3,19 @@ import argparse
 import logging
 import time
 import string
-import matplotlip.pyplot as plt
+#import matplotlip.pyplot as plt
 
 parser = argparse.ArgumentParser(description='measure the relative frequency of the letters of the alphabet in a text')
-parser.add_argument("testo", help="prende come stringa il nome del file da analizzare")
-parser.add_argument('-isto', '--istogramma', help='print the histogram of the frequences", action="store_true')
+parser.add_argument('testo', help='prende come stringa il nome del file da analizzare')
+parser.add_argument('-isto', '--istogramma', help='print the histogram of the frequences', action='store_true')
 args= parser.parse_args()
 
+def from_txt_to_str(txt=None):
+    file = open(txt,'r')
+    file.seek(0)
+    file.close
+    return file.read()
 
+test=from_txt_to_str(args.testo)
 dic = { key : 0 for key in string.ascii_lowercase }
 
